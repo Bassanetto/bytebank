a69d0d9ff51637272485531b9683cf4ada56fef4
Gustavo Bassanetto <gustavo.almeidabassanetto@gmail.com>
2023-02-10 11:58:41 -0300

    C# Orientaçãp a Objetos :: Composição da classe

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,12 +1,30 @@
 ﻿using bytebank.models;
 
-ContaCorrente contaAndre = new ContaCorrente();
-contaAndre.Agencia = 15;
-contaAndre.Titular.Nome = "André Silva";
-contaAndre.Conta = "1010-X";
-contaAndre.Saldo = 100;
+// ContaCorrente contaAndre = new ContaCorrente();
+// contaAndre.Agencia = 15;
+// contaAndre.Titular.Nome = "André Silva";
+// contaAndre.Conta = "1010-X";
+// contaAndre.Saldo = 100;
 
-Console.WriteLine("Saldo da conta do André = " + contaAndre.Saldo);
+Cliente cliente = new Cliente();
+
+cliente.Nome = "André Silva";
+cliente.Cpf = "123456789";
+cliente.Profissao = "Analista";
+
+ContaCorrente conta = new ContaCorrente();
+
+conta.Titular = cliente;
+conta.Conta = "1010-X";
+conta.Agencia = 15;
+conta.Saldo = 100;
+
+Console.WriteLine("Titular: " + conta.Titular.Nome);
+Console.WriteLine("CPF: " + conta.Titular.Cpf);
+Console.WriteLine("Profissão: " + conta.Titular.Profissao);
+Console.WriteLine("Agência: " + conta.Agencia);
+Console.WriteLine("Número Conta: " + conta.Conta);
+Console.WriteLine("Saldo: " + conta.Saldo);
 
 ContaCorrente contaMaria = new ContaCorrente();
 contaMaria.Agencia = 20;
@@ -14,13 +32,14 @@ contaMaria.Titular.Nome = "Maria Eduarda";
 contaMaria.Conta = "1010-5";
 contaMaria.Saldo = 350;
 
-Console.WriteLine("Saldo da conta da Maria = " + contaMaria.Saldo);
+// Console.WriteLine("Saldo da conta da Maria = " + contaMaria.Saldo);
 
-contaAndre.Transferir(50, contaMaria);
+// contaAndre.Transferir(50, contaMaria);
 
-System.Console.WriteLine("Saldo do André = " + contaAndre.Saldo);
-System.Console.WriteLine("Saldo da Maria = " + contaMaria.Saldo);
+// System.Console.WriteLine("Saldo do André = " + contaAndre.Saldo);
+// System.Console.WriteLine("Saldo da Maria = " + contaMaria.Saldo);
 
 ContaCorrente contaPedro = new ContaCorrente();
 
 contaPedro.Titular.Nome = "Pedro Almeida";
+
