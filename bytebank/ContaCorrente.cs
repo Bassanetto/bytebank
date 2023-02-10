03a6ee61ed5d9d48104de35c9acfa04f21b7c45f
Gustavo De Almeida Bassanetto <gustavo.almeidabassanetto@gmail.com>
2023-02-09 22:06:13 -0300

    C# Orientação a Objetos :: Adição do método Deposito

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -2,5 +2,13 @@ namespace bytebank;
 
 public class ContaCorrente
 {
-    
+    public int Agencia;
+    public string Conta;
+    public string Titular;
+    public double Saldo;
+
+    public void Deposito(double valor)
+    {
+        this.Saldo += valor;
+    }
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using bytebank;
+
+ContaCorrente contaAndre = new ContaCorrente();
+contaAndre.Agencia = 15;
+contaAndre.Titular = "André Silva";
+contaAndre.Conta = "1010-X";
+contaAndre.Saldo = 100;
+
+Console.WriteLine("Saldo da conta do André = " + contaAndre.Saldo);
+
+contaAndre.Deposito(100);
+
+Console.WriteLine("Saldo da conta do André pós deposito = " + contaAndre.Saldo);
